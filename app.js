e0f2a706b2f66fbe9d2aadf24a50d915d35c9216
Anna Williamson <acwilliamson97@gmail.com>
2020-06-11 21:39:39 -0700

    test app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,11 +10,130 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const employee = [];
+
+promptManager();
+function promptManager() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the name of your manager?",
+                name: "managerName"
+            },
+            {
+                type: "input",
+                message: "What is your manager's ID number?",
+                name: "managerId"
+            },
+            {
+                type: "input",
+                message: "What is your manager's email?",
+                name: "managerEmail"
+            },
+            {
+                type: "input",
+                message: "What is your manager's office number?",
+                name: "managerOffice"
+            }
+        ])
+        .then(answer => {
+            const newManager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.managerOffice)
+            company.push(newManager)
+            employeeType();
+        })
+}
+
+function addEmployee(job) {
+    if (job === "Engineer") {
+        addEngineer()
+    } else if (job === "Intern") {
+        addIntern()
+    } else {
+        writeFile();
+    }
+}
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                message: `What is the name of your engineer?`,
+                name: `name`
+            },
+            {
+                message: `What is your engineer's ID number?`,
+                name: `id`
+            },
+            {
+                message: `What is your engineer's email?`,
+                name: `email`
+            },
+            {
+                message: `What is your engineer's GitHub username?`,
+                name: `github`
+            }
+        ])
+        .then(answers => {
+            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github)
+            company.push(engineer);
+            employeeType()
+        })
+}
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                message: `What is the name of your intern?`,
+                name: `name`
+            },
+            {
+                message: `What is your intern's ID number?`,
+                name: `id`
+            },
+            {
+                message: `What is your intern's email?`,
+                name: `email`
+            },
+            {
+                message: `What school does your intern currently attend or graduate from?`,
+                name: `school`
+            }
+        ])
+        .then(answers => {
+            const intern = new Intern(answers.name, answers.id, answers.email, answers.school)
+            company.push(intern);
+            employeeType()
+        })
+}
+
+function employeeType() {
+    inquirer
+        .prompt(
+            [{
+                type: "list",
+                message: "What kind of Employee would you like to add?",
+                choices: ["Engineer", "Intern", "I don't want to add any more team members."],
+                name: "type"
+            }],
+        )
+        .then(answers => {
+            createEmployee(answers.type);
+        })
+}
+
+function writeFile() {
+    fs.writeFile(outputPath, render(company), err => {
+        if (err) {
+            console.log(err);
+        }
+        console.log("Done writing file: " + outputPath);
+    });
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -23,10 +143,6 @@ const render = require("./lib/htmlRenderer");
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
